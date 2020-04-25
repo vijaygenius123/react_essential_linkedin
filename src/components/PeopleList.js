6b413384d19cb73a7c12a78f2a0d6c12d6c9e213
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-04-25 22:19:10 +0530

    Added Form For Filtering

--- src/components/PeopleList.js
+++ src/components/PeopleList.js
@@ -5,6 +5,7 @@ import Person from './Person';
 const PeopleList = () => {
 
     const [people, setpeople] = useState([])
+    const [name, setname] = useState('')
     const [loading, setloading] = useState(true)
 
     useEffect(() => {
@@ -16,17 +17,27 @@ const PeopleList = () => {
             })
     }, [])
 
+    const handleInput = (e) => {
+        setname(e.target.value)
+    }
+
     return (
-        <div className="people-list">
-            {
-                loading && <h2>Loading...</h2>
-            }
-            {
-                people && people.map(person =>
-                    <Person key={person.login.uuid} {...person} />
-                )
-            }
-        </div >
+        <div >
+            <div className="filter-form">
+                <label htmlFor="filter">Filter</label>
+                <input type="text" name="filter" id="filter" onChange={e => handleInput} />
+            </div>
+            <div className="people-list">
+                {
+                    loading && <h2>Loading...</h2>
+                }
+                {
+                    people && people.map(person =>
+                        <Person key={person.login.uuid} {...person} />
+                    )
+                }
+            </div >
+        </div>
     )
 }
 
